0d5cd89180194ada4d31418cc049cb5e50238490
Flavio Batista <flaviobatistabjj@gmail.com>
2024-07-21 14:43:57 -0300

    meu app clima tempo

--- scrpit.js
+++ scrpit.js
@@ -1,1 +1,48 @@
-//263be6f9153edaceed23a4a11600b4c9
+//variaveis e seleçao de elementos
+const apiKey ="263be6f9153edaceed23a4a11600b4c9";
+const apiCoutryURL = "https://countryflagsapi.com/png/";
+
+const cityInput = document.querySelector("#city-input");
+const searchBtn = document.querySelector("#search");
+
+const cityElement = document.querySelector("#city")
+const tempElement = document.querySelector("#temperature span")
+const descElement = document.querySelector("#description")
+const weatherIconElement = document.querySelector("#weather-icon")
+const countryElement = document.querySelector("#country")
+const umidityElement = document.querySelector("#umidity span")
+const windElement = document.querySelector("#wind span")
+
+//funçoes
+const getWeatherData = async(city) => {
+
+    const apiWeatherURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&unit=metric&appid=${apiKey}&lang=pt-br`
+    
+    const res = await fetch(apiWeatherURL);
+    const data = await res.json();
+
+    return data;
+
+};
+
+
+
+const showWeatherData = async (city) => {
+    const data = await getWeatherData(city);
+
+    cityElement.innerText = data.name;
+};
+
+
+
+
+//eventos
+searchBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+
+  const city = cityInput.value;
+
+  showWeatherData(city);
+
+
+});
